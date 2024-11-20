3e8b6d2bae3d813dde8fbd3f33d3c7be87546936
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-20 11:08:10 -0300

    feat: inserting data into the simulation table - option 1

--- Samuel/simulados.sql
+++ Samuel/simulados.sql
@@ -1,11 +1,17 @@
--- PRONTA, SEM ALTERAÇÕES
+-- EM ANDAMENTO (INSERÇÃO DOS DADOS) --
 
+-- CRIAÇÃO DAS TABELAS --
+
+-- TABELA DE SIMULADOS --
 CREATE TABLE simulados (
     id SERIAL PRIMARY KEY,
     titulo VARCHAR(100) NOT NULL,
+    dificuldade VARCHAR(100) NOT NULL,
+    questoes INT NOT NULL,
     tipo VARCHAR(100) NOT NULL
-)
--- TABELA DE QUESTÕES
+);
+
+-- TABELA DE QUESTÕES --
 CREATE TABLE questoes (
     id_questao SERIAL PRIMARY KEY,
     enunciado TEXT NOT NULL,
@@ -16,7 +22,18 @@ CREATE TABLE questoes (
     alternativa_e TEXT NOT NULL,
     resposta_correta VARCHAR(1) NOT NULL,
     id_simulado INT NOT NULL,
-
+    -- CHAVE ESTRANGEIRA --
     FOREIGN KEY (id_simulado) REFERENCES simulados(id)
-)
+);
 
+-- INSEÇÕES NAS TABELAS:
+
+-- INSERTS - SIMULADOS - OPÇÃO 1 - JÁ FORAM TESTADOS --
+INSERT INTO simulados (titulo, dificuldade, questoes, tipo) VALUES 
+('Mergulhe no conhecimento - Questões', 'Fácil', 10, 'Questões de vestibulares'),
+('Para Gabaritar! - Questões', 'Intermediário', 10, 'Questões de vestibulares'),
+('Para Gabaritar! - Questões', 'Básico', 10, 'Questões de vestibulares'),
+('Para Gabaritar! - Questões', 'Difícil', 10, 'Questões de vestibulares'),
+('Para Gabaritar! - Questões', 'Avançado', 10, 'Questões de vestibulares');
+-- TESTE:
+SELECT * FROM simulados;
