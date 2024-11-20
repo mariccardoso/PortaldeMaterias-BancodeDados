047aa99532c440e13fbd47e7e102e1c95156bcdd
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-20 10:29:14 -0300

    feat: creation of tool data inserts and correction of some syntax errors

--- Samuel/home.sql
+++ Samuel/home.sql
@@ -1,16 +1,16 @@
-
+-- EM ANDAMENT0 (INSERÇÃO DOS DADOS)    
 CREATE TABLE paginas_portal (
     id SERIAL PRIMARY KEY,
     titulo VARCHAR(100) NOT NULL,
     url VARCHAR(100) NOT NULL
-)
+);
 
 CREATE TABLE ferramentas (
     id SERIAL PRIMARY KEY,
     ferramenta VARCHAR(100) NOT NULL,
     descricao TEXT NOT NULL,
-    url_img INT NOT NULL
-)
+    url_img TEXT NOT NULL
+);
 
 
 CREATE TABLE contato (
@@ -18,4 +18,18 @@ CREATE TABLE contato (
     nome_completo VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     mensagem TEXT NOT NULL
-)
+);
+
+-- INSERTS
+
+-- FERRAMENTAS
+INSERT INTO ferramentas (ferramenta, descricao, url_img) VALUES 
+('Omni Calculator', 'Calculadoras para matemática e ciências.', 'https://portal-de-materias.vercel.app/assets/img/logos/ominiCalculator.png'),
+('Ptable', 'Tabela periódica interativa com dados químicos.', 'https://portal-de-materias.vercel.app/assets/img/logos/ptable.png'),
+('Symbolab', 'Solucionador de equações matemáticas.', 'https://portal-de-materias.vercel.app/assets/img/logos/symbolab.png'),
+('Notion', 'Ferramenta de produtividade e organização.', 'https://portal-de-materias.vercel.app/assets/img/logos/notion.png'),
+('Google', 'Busca e serviços online.', 'https://portal-de-materias.vercel.app/assets/img/logos/google.png'),
+('Quizlet', 'Flashcards e recursos de aprendizado.', 'https://portal-de-materias.vercel.app/assets/img/logos/quizlet.png');
+
+-- TESTE:
+SELECT * FROM ferramentas;
