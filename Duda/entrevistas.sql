7d8bdcc08b5fe28ae799dba269022602fa12c9d9
maria Eduarda <mariaeduardareistds2.senai@gmail.com>
2024-11-26 09:10:46 -0300

    feat: create of inserts for the interviewed table

--- Duda/entrevistas.sql
+++ Duda/entrevistas.sql
@@ -6,3 +6,9 @@ CREATE TABLE entrevistas (
 
     FOREIGN KEY (id_entrevistado) REFERENCES especialistas(id)
 )
+
+INSERT INTO entrevistas (titulo, descricao, id_entrevistado) VALUES
+('Tragetória Profissional de Andressa Bruscato', 'Andressa Bruscato é mestre em Bioquímica e conta um pouco sobre sua trajetória profissional.', 1),
+('Como escolher minha área na química?', 'Andressa compartilha um pouco sobre sua experiência e dá dicas para quem deseja seguir carreira na área da química.', 1),
+('⁠Se preparando para vestibulares', 'Andressa Bruscato dá dicas para quem está se preparando para vestibulares e ENEM.', 1),
+('Como o portal de química te ajuda no mercado de trabalho', 'João Borghi, estudante do SESI/SENAI, conta como o portal de química o ajudou no mercado de trabalho.', 2);
